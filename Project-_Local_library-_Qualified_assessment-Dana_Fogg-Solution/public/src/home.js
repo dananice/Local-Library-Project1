828f651b57491349435c5943e203a6383975475f
Dana Fogg <102395872+dananice@users.noreply.github.com>
2022-04-09 14:57:23 -0400

    Update home.js

--- Project-_Local_library-_Qualified_assessment-Dana_Fogg-Solution/public/src/home.js
+++ Project-_Local_library-_Qualified_assessment-Dana_Fogg-Solution/public/src/home.js
@@ -1,14 +1,45 @@
-function getTotalBooksCount(books) {}
+function getTotalBooksCount(books) {
+/ return number of books in array   
+ for (let i = 0; i < books.length; i++)
+ 
 
-function getTotalAccountsCount(accounts) {}
 
-function getBooksBorrowedCount(books) {}
+}
 
-function getMostCommonGenres(books) {}
+function getTotalAccountsCount(accounts) {
+// return number of account objects inside the array
 
-function getMostPopularBooks(books) {}
+}
 
-function getMostPopularAuthors(books, authors) {}
+function getBooksBorrowedCount(books) {
+// returns number of books that are currently checked out 
+  // of the library
+
+}
+
+function getMostCommonGenres(books) {
+// returns an array with 5 objects or less that represents
+  // the most common occurring genres ordered from most common 
+  // to least
+  
+
+
+}
+
+function getMostPopularBooks(books) {
+// returns array containing 5 objects or less that represents
+// the most popular books in the library
+
+
+
+}
+
+function getMostPopularAuthors(books, authors) {
+//returns array containing 5 objects or fewer that represents 
+ //the most popular authors whose books have been  checked
+ //out the most 
+
+}
 
 module.exports = {
   getTotalBooksCount,
