484ad6ef5bf469cfe799d73026ee5beba5d1d645
Dana Fogg <102395872+dananice@users.noreply.github.com>
2022-04-09 14:54:59 -0400

    Update books.js

--- Project-_Local_library-_Qualified_assessment-Dana_Fogg-Solution/public/src/books.js
+++ Project-_Local_library-_Qualified_assessment-Dana_Fogg-Solution/public/src/books.js
@@ -1,10 +1,29 @@
-function findAuthorById(authors, id) {}
+function findAuthorById(authors, id) {
+ // returns the author object that has the matching id 
 
-function findBookById(books, id) {}
 
-function partitionBooksByBorrowedStatus(books) {}
+}
 
-function getBorrowersForBook(book, accounts) {}
+function findBookById(books, id) {
+//returns the book object with matching id
+
+}
+
+function partitionBooksByBorrowedStatus(books) {
+// returns array w/ 2 arrays inside of it all the inputted 
+// books are present in either the 1st or 2nd array
+//1st array contains book currently checked out
+//2nd array contains books that have been returned
+
+}
+
+function getBorrowersForBook(book, accounts) {
+//return an array of ten or fewer account objects that 
+ //represents the accounts given by the ID in the provided
+ //books borrows array each account should incl the returned 
+ // entry from the corresponding transactions object in the 
+ //borrows array  
+}
 
 module.exports = {
   findAuthorById,
