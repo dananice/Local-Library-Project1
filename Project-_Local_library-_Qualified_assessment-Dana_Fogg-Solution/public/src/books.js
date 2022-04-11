2dad8c46122cc646f327e799d343780e0b00cfa6
Dana Fogg <102395872+dananice@users.noreply.github.com>
2022-04-10 22:23:43 -0400

    Update books.js

--- Project-_Local_library-_Qualified_assessment-Dana_Fogg-Solution/public/src/books.js
+++ Project-_Local_library-_Qualified_assessment-Dana_Fogg-Solution/public/src/books.js
@@ -1,12 +1,14 @@
 function findAuthorById(authors, id) {
  // returns the author object that has the matching id 
- return authors.filter(authors => authors.id === id);
-
+ return authors.find(authors => authors.id === id);
 }
 
 function findBookById(books, id) {
-//returns the book object with matching id 
- return books.filter(books => books.id === id);
+ //returns the book object with matching id
+
+ return books.find(books => books.id === id);
+
+
 }
 
 function partitionBooksByBorrowedStatus(books) {
@@ -15,15 +17,31 @@ function partitionBooksByBorrowedStatus(books) {
 //1st array contains book currently checked out
 //2nd array contains books that have been returned
 
+let booksReturned = books.filter((book) =>
+  book.borrows.every((borrow) => borrow.returned === true)
+ );
+ let booksBorrowed = books.filter((book) =>
+  book.borrows.some((borrow) => borrow.returned === false)
+ );
+ let secondArray = [[...booksBorrowed], [...booksReturned]];
+ return secondArray;
 }
 
 function getBorrowersForBook(book, accounts) {
-//return an array of ten or fewer account objects that 
+ //return an array of ten or fewer account objects that 
  //represents the accounts given by the ID in the provided
  //books borrows array each account should incl the returned 
  // entry from the corresponding transactions object in the 
  //borrows array  
-}
+
+ function getBorrowersForBook(book, accounts) {
+    return book.borrows
+     .map((borrow) => {
+      let account = accounts.find((account) => account.id === borrow.id);
+      return { ...borrow, ...account };
+     })
+     .slice(0, 10);
+
 
 module.exports = {
   findAuthorById,
